8be498ab2c764cc8c9892c779d2d05907036829e
Devansh Agarwal <devanshagarwal@pop-os.localdomain>
2022-10-08 00:54:36 +0530

    Starting to learn Spring5

---
+++ src/main/java/dev/springframework/startSpring5webapp/StartSpring5webappApplication.java
@@ -0,0 +1,13 @@
+package dev.springframework.startSpring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartSpring5webappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/springframework/startSpring5webapp/StartSpring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.springframework.startSpring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartSpring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
